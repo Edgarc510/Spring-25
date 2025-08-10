fb4a17e9db9946d533155992b2527a25858fe50f
ceale <cealex510@gmail.com>
2025-08-10 07:40:31 -0700

    Created base classes

---
+++ Spring-05-Core-BeanScope/src/main/java/com/eac/proxy/EmailCommentNotificationProxy.java
@@ -0,0 +1,15 @@
+package com.eac.proxy;
+
+import com.eac.model.Comment;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+@Qualifier("EMAIL")
+public class EmailCommentNotificationProxy implements CommentNotificationProxy{
+
+    @Override
+    public void sendComment(Comment comment){
+        System.out.println("Sending notification for " + comment.getText());
+    }
+}
