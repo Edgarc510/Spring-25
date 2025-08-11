a647039160a8890cb9cb70cc8e85280ae7214292
ceale <cealex510@gmail.com>
2025-08-11 16:24:40 -0700

    Added main spring boot folder structure

---
+++ Spring-06-SpringBoot-Demo/src/main/java/com/eac/Spring_06_SpringBoot_Demo/Spring06SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.eac.Spring_06_SpringBoot_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring06SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring06SpringBootDemoApplication.class, args);
+	}
+
+}
