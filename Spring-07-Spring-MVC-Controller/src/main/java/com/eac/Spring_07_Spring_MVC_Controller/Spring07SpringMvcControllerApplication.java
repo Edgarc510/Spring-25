e06fee4e536c8ac7b343c552c2eedcd12654fe86
ceale <cealex510@gmail.com>
2025-08-12 16:38:08 -0700

    spring mvc initial set up

---
+++ Spring-07-Spring-MVC-Controller/src/main/java/com/eac/Spring_07_Spring_MVC_Controller/Spring07SpringMvcControllerApplication.java
@@ -0,0 +1,13 @@
+package com.eac.Spring_07_Spring_MVC_Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring07SpringMvcControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring07SpringMvcControllerApplication.class, args);
+	}
+
+}
