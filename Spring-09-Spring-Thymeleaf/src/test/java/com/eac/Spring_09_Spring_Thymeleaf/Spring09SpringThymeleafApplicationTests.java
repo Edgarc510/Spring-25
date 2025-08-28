004ff52a5bbbf719a8a3ec25640ff3d3cee93847
ceale <cealex510@gmail.com>
2025-08-28 16:29:11 -0700

    spring thymeleaf initial set-up

---
+++ Spring-09-Spring-Thymeleaf/src/test/java/com/eac/Spring_09_Spring_Thymeleaf/Spring09SpringThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eac.Spring_09_Spring_Thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring09SpringThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
